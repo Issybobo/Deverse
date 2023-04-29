ea2a0df5dc9f8db213d2fceb0b4f13688c46d968
Issybobo <Israelolamideogunsola75@gmail.com>
2023-04-29 19:47:31 +0100

    first

---
+++ frontend/constants/index.js
@@ -0,0 +1,84 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-06.png',
+    title: 'The Acheivers',
+  },
+ 
+  {
+    id: 'world-3',
+    imgUrl: '/planet-09.png',
+    title: 'Arena Villa',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Paris villa',
+  },
+];
+
+export const startingFeatures = [
+  'Discover a realm that resonates with your desires and that you aspire to enter.',
+  'Enter the world by using VR and be safe',
+  'Cut to the chase, step on the gas, and enjoy the ride.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/headset.svg',
+    title: 'A new world',
+    subtitle:
+        'The world has been updated with a new version for you to explore, disregard the previous one',
+  },
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Realistic',
+    subtitle:
+        'With the latest update, the world appears more realistic than ever before as your eyes squint',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-02.png',
+    title: 'The launch of the Metaverse makes the World a better place',
+    subtitle:
+        'It is an immersive environment that allows users to experience a digital world that is similar to the physical world in some ways.',
+  },
+  {
+    imgUrl: 'planet-04.png',
+    title: 'Metaverse is the future ',
+    subtitle:
+        'the metaverse is an exciting and rapidly evolving field that could have significant implications for how we live, work, and play in the future.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'In the metaverse, users can create avatars that represent them in the virtual world and can interact with other users,',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ frontend/sections/About.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    > 
+      <TypingText title=" | About Deverse " textStyles="text-center" textFont = "font-extrabold" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Metaverse</span> is a virtual world where users can interact with a computer-generated
+         environment and with each other through avatars or digital representations of themselves. It's often described as a fully immersive, shared,
+         and persistent virtual space that can be accessed from different devices, such as computers, virtual reality headsets, or smartphones.{' '}
+        <span className="font-extrabold text-white">
+          
+        </span>{' '}
+      
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
